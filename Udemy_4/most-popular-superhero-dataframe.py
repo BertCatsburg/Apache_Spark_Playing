1c91b05f5a0b08aa913217bde14b311d69c08367
bert@catsburg.com <bert@catsburg.com>
2021-11-14 20:44:54 +0100

    Most Popular Super Hero

--- Udemy_4/most-popular-superhero-dataframe.py
+++ Udemy_4/most-popular-superhero-dataframe.py
@@ -5,20 +5,25 @@ from pyspark.sql.types import StructType, StructField, IntegerType, StringType
 # Create the SparkSession
 spark = SparkSession.builder.appName("MostPopularSuperhero").getOrCreate()
 
+# Schema for the Names
 schema = StructType([
                      StructField("id", IntegerType(), True),
                      StructField("name", StringType(), True)])
 
+# Read in the Names
 names = spark.read.schema(schema).option("sep", " ").csv("file:///sparkdata/marvel/names.txt")
 
-lines = spark.read.text("file:///SparkCourse/Marvel-graph.txt")
+# The Graph data in a Dataframe
+lines = spark.read.text("file:///sparkdata/marvel/graph.txt")
+# lines.sort('value').show()
 
-# Small tweak vs. what's shown in the video: we trim each line of whitespace as that could
-# throw off the counts.
-connections = lines.withColumn("id", func.split(func.trim(func.col("value")), " ")[0]) \
+# Trim each line of whitespace as that could throw off the counts.
+# - Add column id. Value is
+connections = lines \
+    .withColumn("id", func.split(func.col("value"), " ")[0]) \
     .withColumn("connections", func.size(func.split(func.trim(func.col("value")), " ")) - 1) \
     .groupBy("id").agg(func.sum("connections").alias("connections"))
-    
+
 mostPopular = connections.sort(func.col("connections").desc()).first()
 
 mostPopularName = names.filter(func.col("id") == mostPopular[0]).select("name").first()
